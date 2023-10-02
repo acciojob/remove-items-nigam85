107bb9230612e78f9c6ccd05e3153fb6f0229673
Anish Nigam <117471360+nigam85@users.noreply.github.com>
2023-10-02 14:37:50 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+const removeButton = document.getElementById('removeButton');
+        const colorSelect = document.getElementById('colorSelect');
+
+        removeButton.addEventListener('click', function() {
+            const selectedIndex = colorSelect.selectedIndex;
+            if (selectedIndex !== -1) { // Check if an option is selected
+                colorSelect.remove(selectedIndex); // Remove the selected option
+            }
+        });
+
